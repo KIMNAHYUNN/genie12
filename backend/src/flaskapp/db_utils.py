8e32373c5cfca3c5ddf28aa9e6e152b25c9395ab
Dayeol <dyjo.seoul@gmail.com>
2021-12-29 11:10:10 +0900

    frontend and backend merged

--- backend/src/flaskapp/db_utils.py
+++ backend/src/flaskapp/db_utils.py
@@ -1,5 +1,8 @@
 import pymysql
 from flask import g
+from PIL import Image
+from io import BytesIO
+
 def connect_db():
     try:
         db = pymysql.connect(
@@ -8,7 +11,6 @@ def connect_db():
             user='tarotadm',
             passwd='tarotadm123!',
             db='tarot', charset='utf8', autocommit=True)
-
         return db
     except Exception as ex:
         print("Can't connect to database")
@@ -23,13 +25,51 @@ def secure_query(base_query, var_tuple):
     database = get_db()
 
     with database.cursor() as cursor:
-        query = cursor.mogrify(base_query, var_tuple)
-        cursor.execute(query)
+        # query = cursor.mogrify(base_query, var_tuple)
+        cursor.execute(base_query, var_tuple)
 
-        if b'SELECT' in query or b'RETURNING' in query:
-            result = cursor.fetchall()
-        else:
-            result = None
+        # if b'SELECT' in query or b'RETURNING' in query:
+        #
+        result = cursor.fetchall()
+        # else:
+        #     result = None
         database.commit()
 
         return result
+
+def save_user_info(tarot_id, emotion_id):
+    user_query = "UPDATE IT_USER_MAS SET TAROT_ID=%s, EMOTION_ID=%s WHERE USER_ID='TEST0001'"
+    secure_query(user_query, (tarot_id, emotion_id))
+
+def save_fortune_id(fortune_id):
+    user_query = "UPDATE IT_USER_MAS SET fortune_id=%s WHERE USER_ID='TEST0001'"
+    secure_query(user_query, [fortune_id])
+
+def get_fortune_info(tarot_id, emotion_id):
+    fortune_desc_query = "SELECT FORTUNE_DESC, FORTUNE_ID FROM IT_FORTUNE_MAS WHERE TAROT_ID=%s AND EMOTION_ID=%s"
+    fortune = secure_query(fortune_desc_query, (tarot_id, emotion_id))[0][0]
+    fortune_info = {
+        'fortune_desc': fortune[0][0],
+        'fortune_id': fortune[0][1]
+    }
+    return fortune_info
+
+def get_tarot_info(tarot_id):
+    tarot_info_query = "SELECT tarot_nm, image_data FROM IT_TAROT_MAS WHERE TAROT_ID=%s"
+    tarot_info = secure_query(tarot_info_query, [tarot_id])
+    tarot_nm = tarot_info[0][0]
+    image_data = tarot_info[0][1]
+
+    get_image = Image.open(BytesIO(image_data['image_data']))
+    get_image = get_image.decode("UTF-8")
+
+    tarot_info = {
+        'tarot_nm':tarot_nm,
+        'image_data':get_image
+    }
+    return tarot_info
+
+
+
+
+
--- backend/src/flaskapp/views.py
+++ backend/src/flaskapp/views.py
@@ -1,5 +1,6 @@
-from flask import request, g, Response, jsonify
+from flask import request, g, Response, jsonify, render_template
 from flaskapp import app
+from flaskapp.db_utils import *
 
 @app.teardown_appcontext
 def close_db(e=None):
@@ -10,4 +11,43 @@ def close_db(e=None):
 
 @app.route('/')
 def route_main():
-    return "hello :)"
+    return render_template('index.html')
+
+@app.route('/start')
+def route_start():
+    return render_template('start.html')
+
+@app.route('/luck')
+def route_luck():
+    return render_template('luck.html')
+
+@app.route('/card')
+def route_card():
+    return render_template('card.html')
+
+@app.route('/result')
+def route_result():
+    return render_template('result.html')
+
+@app.route('/review')
+def route_review():
+    return render_template('review.html')
+
+@app.route('/exit')
+def route_exit():
+    return render_template('exit.html')
+
+@app.route('/tarot', methods=['GET'])
+def route_tarot():
+    data = request.headers
+    try:
+        tarot_id = data.get("tarot_id")
+        emotion_id = data.get("emotion_id")
+    except:
+        return Response(status=500)
+    save_user_info(tarot_id, emotion_id)
+    # tarot_name = get_tarot_mas(tarot_id)
+    #
+    # return jsonify({
+    #     'tarot_name':tarot_name
+    # })
