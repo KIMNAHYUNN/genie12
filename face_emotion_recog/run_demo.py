825924107271395eb5142a28c0a457f3e74f98bd
Geunuk <geunuking@gmail.com>
2022-01-01 00:17:35 +0900

    add new model

--- face_emotion_recog/run_demo.py
+++ face_emotion_recog/run_demo.py
@@ -1,9 +1,14 @@
+from pathlib import Path 
+    
 import numpy as np
 import cv2 as cv
 
 from emotion_recog import load_fer_model, inference
 
+# Fact detection 모델 위치
 detect_model_path = "./haarcascades_model/haarcascade_frontalface_default.xml"
+
+# 얼굴 부분에 칠할 박스의 색깔
 emotion_to_color = {
     "angry":    [255,   0,   0], # Red
     "disgust":  [  0, 255,   0], # Green
@@ -13,14 +18,17 @@ emotion_to_color = {
     "sad":      [  0,   0, 128], # Blue
     "surprise": [255, 255,   0]  # Yellow
 }
+
 FER_MODEL_INPUT_SHAPE = (48, 48)
 
 def draw_bounding_box(coords, img, color):
+    """얼굴 부분에 박스 그리기"""
     thickness = 2
     x, y, w, h = coords
     cv.rectangle(img, (x, y), (x + w, y + h), color, thickness) 
 
 def draw_text(coords, img, text, color):
+    """얼굴 박스 옆에 감정 글씨 넣기"""
     x, y, h, w = coords
     org = (x, y-10) # Bottom left position of the string
     font_scale = 1
@@ -31,16 +39,17 @@ def draw_text(coords, img, text, color):
                font_scale, color, thickness, cv.LINE_AA)
 
 def main():
+    # 얼굴 감지 모델, 감정 분류 모델 불러오기
     detect_model = cv.CascadeClassifier(detect_model_path)
-    fer_model = load_fer_model("./models/211229-085713-0.5362/model.pt")
+    fer_model = load_fer_model(Path("./models/211231-100640-0.5086-bs_256_lr_0.001_mom_0.99_eps_0.001/")) # 수정
 
-    cap = cv.VideoCapture(0) # Device index is 0
+    cap = cv.VideoCapture(0) # 웹 캠 불러오기
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
-    cv.namedWindow("Demo")
+
+    cv.namedWindow("Demo") # 화면에 웹 캠 창 띄우기
     while True:
-        # Capture frame-by-frame
         ret, bgr_img = cap.read()
         # If frame is read correctly ret is True
         if not ret:
@@ -54,14 +63,17 @@ def main():
         min_neighbors = 5
         faces = detect_model.detectMultiScale(gray_img, scale_factor, min_neighbors)
         for face_coords in faces:
+            # 얼굴의 위치 좌표: (x1, y1) 좌상단, (x2, y2) 우하단
             x, y, h, w = face_coords
-            x1, x2, y1, y2 = x, x+w, y, y+h
+            x1, x2, y1, y2 = x, x+w, y, y+h 
             
+            # 감정 분류 모델에 넣을 수 있도록 크기 변환
             gray_face = gray_img[y1:y2, x1:x2]
             gray_face = cv.resize(gray_face, FER_MODEL_INPUT_SHAPE)
 
             emotion, prob, is_pos_emo = inference(fer_model, gray_face)
             
+            # 얼굴 부분에 박스 및 감정 표시
             color = emotion_to_color[emotion]
             draw_bounding_box(face_coords, rgb_img, color)
             draw_text(face_coords, rgb_img, f"{emotion}", color)
